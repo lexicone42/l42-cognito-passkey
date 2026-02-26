18a850ee4e3d4d9cf1c355ac1d7a09d9b7159999
bryan <bryan.egan@gmail.com>
2026-02-25 16:58:52 -0800

    0.20.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to this project will be documented in this file.
 
+## [0.20.1] - 2026-02-26
+
+_Release notes pending._
+
 ## [0.20.0] - 2026-02-21
 
 ### Added
--- src/auth.js
+++ src/auth.js
@@ -8,11 +8,11 @@
  *   import { configure, isAuthenticated, loginWithPassword } from './auth.js';
  *   configure({ clientId: 'xxx', cognitoDomain: 'xxx.auth.region.amazoncognito.com' });
  *
- * @version 0.20.0
+ * @version 0.20.1
  * @license Apache-2.0
  */
 
-export const VERSION = '0.20.0';
+export const VERSION = '0.20.1';
 
 // ==================== CONFIGURATION ====================
 
